ef3817bfc90eb2959ef68bd9dda2bca3dc03503c
Alishakoori <alishakoori89@gmail.com>
2021-07-11 15:02:35 +0430

    edit code

--- lib/view/city_weather_details_with_citylocation.dart
+++ lib/view/city_weather_details_with_citylocation.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:flutter_spinkit/flutter_spinkit.dart';
 import 'package:intl/intl.dart';
 import 'package:weather/bloc/search_location_bloc.dart';
 import 'package:weather/view/daily_week_weathers_with_city_location.dart';
